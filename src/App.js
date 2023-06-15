06dcea90d282aaad0603f42deeba8584b2b58804
Nathaniel <nathacasa07@gmail.com>
2023-06-15 02:06:55 -0600

    setting up authentication verification for checkout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationRoute from './components/NavigationRoute';
 
+
 function App() {
   return (
     <NavigationRoute/>
--- src/components/NavigationRoute.js
+++ src/components/NavigationRoute.js
@@ -1,18 +1,44 @@
-import {Routes,Route} from 'react-router-dom';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import Home from '../pages/Home';
 import Checkout from '../pages/Checkout';
 import Login from '../pages/Login';
 import UserProfile from '../pages/UserProfile';
+import { useContext, useEffect, useState } from 'react';
+import userContext, { UserContext } from './context/userContext';
 
 
 function NavigationRoute() {
+    const [user, setUser] = useState({});
+  const [token, setToken] = useState('');
+
+  useEffect(()=>{
+    console.log(userContext);
+  }, [userContext])
+
+
+    const {
+      userContextLogin,
+      setTokenContext,
+      tokenContext,
+      setUserContextLogin
+    } = useContext(UserContext);
     return(
+        <BrowserRouter>
         <Routes>
-            <Route path='/checkout' element={<Checkout/>}/>
-            <Route path='/' element={<Home/>}/>
+          {
+            !user ?
+            <Route path = '/checkout' element={<Login
+              setToken={setToken}
+              setUser={setUser}
+              />}></Route>
+              :
+              <Route path='/chechout' element={<Checkout/>}/>
+          }
+          <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/userprofile' element={<UserProfile/>}/>
         </Routes>
+      </BrowserRouter>
     )
 }
 export default NavigationRoute;
